68548c5d6647a072ae2ca9ea15423a4cd031d1f5
amontague47 <amontague47@massmutual.com>
2018-04-11 17:07:20 -0400

    mySQL connections, API issues

--- Safari-2/todos-api/api/controllers/todosController.js
+++ Safari-2/todos-api/api/controllers/todosController.js
@@ -8,5 +8,39 @@ exports.listTasks = function (req, res) {
         if (err)
             res.send(err);
         res.json(task);
+        //res.write("Hello");
+    });
+};
+
+exports.createTask = function (req, res){
+    var newTask = tasks(req.body);
+    newTask.save(function(err, task){
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.readTask = function (req, res){
+    tasks.findById(req.param.taskId, function(err, task){
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.updateTask = function (req, res){
+    tasks.findOneAndUpdate({_id: req.param.taskId}, req.body, {new: true}, function (err, task){
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.deleteTask = function(req, res){
+    tasks.remove({_id: req.para.taskId}, function (err, task){
+        if (err)
+            res.send(err);
+        res.json({message: 'Task Deleted'});
     });
 };
--- Safari-2/todos-api/server.js
+++ Safari-2/todos-api/server.js
@@ -1,7 +1,33 @@
-var express = require('express'),
+/* var express = require('express'),
     app = express(),
     ip = process.env.ip,
-    port = process.env.port || 8080;
+    port = process.env.port || 8080,
+    mySQL = require('mySQL'),
+    tasks = require('./api/models/todosModel'),
+    bodyParser = require('body-parser');
+
+    mySQL.Promise = global.Promise;
+    mySQL.connect('mySQL://localhost/');
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    var routes = require('./api/routes/todosRoutes');
+    routes(app);
 
     app.listen(port,ip);
-    console.log('The TODO REST API server is running on IP: ' + ip + ' and Port: ' +port);
+    console.log('The TODO REST API server is running on IP: ' + ip + ' and Port: ' +port); */
+
+var http = require('http');
+var dateTimeFromModule = require('./mod1');
+var fromQueryString = require('url');
+var pullFromDB = require('./dbPull');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("The date and time: " + dateTimeFromModule.myDateTime() + "\n");
+    var queryStringData = fromQueryString.parse(req.url, true).query;
+    var txt = queryStringData.item + " " + queryStringData.price;
+    res.write(pullFromDB.theseResults() + "\n");
+    res.end(txt);
+}).listen(8080);
